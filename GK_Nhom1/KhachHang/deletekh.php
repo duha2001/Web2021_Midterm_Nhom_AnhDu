e84a560225eaabfcaad4f4d98278b71df31f4d6c
duha2001 <anhduhuynh2001@gmail.com>
2021-05-28 13:58:16 +0700

    File lỗi xóa

--- GK_Nhom1/KhachHang/deletekh.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $id =$_GET['id'];
-    $sql = "DELETE FROM khachhang where MaKH = $id";
-    $query = mysqli_query($db,$sql);
-
-    header('location: index.php?page_layout=selectkh');
-?>
